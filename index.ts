7ac8640d8aef5e2e822faecda12e94b8e38530c3
Kunal Bagaria <kb24x7@gmail.com>
2023-03-05 00:55:33 +0530

    fixes, bump: version

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-import { Profile, ProfileMetadata } from "@/Profile";
-import { Post, PostMetadata } from "@/Post";
-import { FollowButton, FollowMetadata } from "@/Follow";
+import { Profile, ProfileMetadata } from "./src/Profile";
+import { Post, PostMetadata } from "./src/Post";
+import { FollowButton, FollowMetadata } from "./src/Follow";
 
 export { Profile, ProfileMetadata };
 export { Post, PostMetadata };
